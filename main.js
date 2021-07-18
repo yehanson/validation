74fd2d89e2a7779da891b778538ae2c02ef84b0a
HANSON <80563592+yehanson@users.noreply.github.com>
2021-07-18 07:36:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,36 +2,71 @@ const user= document.querySelector('.user');
 const pw= document.querySelector('.pw');
 const pw2= document.querySelector('.pw2');
 const mail= document.querySelector('.mail');
+const country= document.querySelector('.country');
+const province= document.querySelector('.province');
+const city= document.querySelector('.city');
 const subbtn= document.querySelector('.submit');
 const msg= document.querySelector('.msg');
 
 subbtn.addEventListener('click', check);
+let locationoptions= {
+    'Canada': {
+        'Ontario': ['Toronto', 'Markham'],
+        'British Columbia': ['Vancouver', 'Richmond']
+    },
+    'USA': {
+        'California': ['San Francisco', 'Los Angeles'],
+        'New York': ['New York City', 'Buffalo']
+    }
+};
+
+window.onload= function(){
+    for(let x in locationoptions){
+        country.options[country.options.length]= new Option(x, x);
+    };
+    country.onchange= function(){
+        province.length= 1;
+        city.length= 1;
+        console.log(locationoptions[country.value]);
+        //if object
+        for(let y in locationoptions[country.value]){
+            province.options[province.options.length]= new Option(y, y);
+        };
+    };
+    province.onchange= function(){
+        city.length= 1;
+        console.log(locationoptions[country.value][province.value]);
+        //if array
+        let z= locationoptions[country.value][province.value];
+        for(j= 0; j < z.length; j++){
+            city.options[city.options.length]= new Option(z[j], z[j]);
+        };
+    };
+};
 
 function check(){
-    userv= user.value.trim();
-    pwv= pw.value.trim();
-    pwv2= pw2.value.trim();
-    mailv= mail.value.trim();
+    let userv= user.value.trim();
+    let pwv= pw.value.trim();
+    let pw2v= pw2.value.trim();
+    let mailv= mail.value.trim();
+    let cityv= city.value;
 
     if(userv== ''){
         error(user, 'Username Invalid');
     }else{
         success(user);
-        user.value= '';
     };
 
-    if(pwv== '' || pwv2== ''){
+    if(pwv== '' || pw2v== ''){
         error(pw, 'Password Invalid');
     }else if(pwv.search(/[A-Z\a-z\0-9]/)== -1){
         error(pw, 'Password Does Not Meet Requirements');
     }else if(pwv.length <= 0 || pwv.length > 8){
         error(pw, 'Password Does Not Meet Requirements');
-    }else if(pwv!= pwv2){
+    }else if(pwv != pw2v){
         error(pw, 'Password Does Not Match');
     }else{
         success(pw);
-        pw.value= '';
-        pw2.value= '';
     };
 
     if(mailv== ''){
@@ -40,8 +75,13 @@ function check(){
         error(mail, 'Email Invalid');
     }else{
         success(mail);
-        mail.value= '';
     };
+
+    if(cityv== ''){
+        error(location, 'Location Invalid');
+    }else{
+        console.log('location working');
+    }
 };
 
 function error(input, message){
